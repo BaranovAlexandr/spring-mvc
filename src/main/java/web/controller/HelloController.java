486d0954f69a3e62cd36318779f7ab0f6574f6a9
BaranovAlexandr <baranovale228@gmail.com>
2021-08-24 14:52:28 +0300

    commit

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.UserSerice;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +23,15 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/car")
+	public String car(
+			@RequestParam(value = "count") int count,
+			ModelMap model){
+		List<Car> cars = new UserSerice().cars(count);
+		model.addAttribute("cars", cars);
+		return "car";
 	}
 	
 }
