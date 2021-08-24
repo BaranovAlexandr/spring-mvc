5c57466837c73634074002f3b476a02b3feb331b
BaranovAlexandr <baranovale228@gmail.com>
2021-08-24 14:56:35 +0300

    commit

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,34 @@
+package web.model;
+
+public class Car {
+    String brand;
+    String model;
+    int series;
+
+    public Car(String brand, String model, int series) {
+        this.brand = brand;
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
